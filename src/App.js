525cecf43c18a9d044183804ec3a8f527c4ef47a
Nikhil Vijayan <nkhilv@gmail.com>
2019-08-07 00:09:51 +0100

    chore: clean up

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import './App.css';
-// import Example from './components/hook';
-// import DataLoader from './components/Data';
 import Post from './components/post';
 
 function App() {
   return (
     <div className="App">
-      {/* <Example />
-      <DataLoader /> */}
       <Post />
     </div>
   );
